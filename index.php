6419989efd6301ee2197a412ae9b9e702dc70b5b
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-05-22 15:43:22 +0200

    prima stmpa in pagina

--- index.php
+++ index.php
@@ -54,6 +54,15 @@
 </head>
 <body>
 
+    <div class="container">
+        <h1 class="text-center">HOTELS</h1>
+        <?php foreach($hotels as $elem){ ?>
+                <ul>
+                    <li><?php echo $elem['name'] ?></li>
+                </ul>
+        <?php } ?>
+        <p></p>
+    </div>
 
     <!-- BOOTSTRAP JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
